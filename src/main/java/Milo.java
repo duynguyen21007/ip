e3993d8f3fa428c737ca9921327195b694361df1
Duy Nguyen <duynguyen21007@gmail.com>
2026-08-19 21:09:52 +0800

    Added echo feature

--- src/main/java/Milo.java
+++ src/main/java/Milo.java
@@ -1,15 +1,36 @@
+import java.util.Scanner;
+
 public class Milo {
+    private static final String DIVIDER = "-----------------------------------";
+    private static final String INDENTBLOCK = "                   ";
     public static void main(String[] args) {
         String chatbotName = "Milo";
-        String greeting = "============================================================\n"
+        String greeting = DIVIDER + "\n"
                 + "Hello! I'm " + chatbotName + ".\n"
                 + "How can I help you?\n"
-                + "============================================================\n";
+                + DIVIDER;
         String banner = " __  __ _ _       \n"
                       + "|  \\/  (_) | ___  \n"
                       + "| |\\/| | | |/ _ \\ \n"
                       + "| |  | | | | (_) |\n"
                       + "|_|  |_|_|_|\\___/ \n";
         System.out.println(banner + greeting);
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (scanner.hasNextLine()) {
+                String command = scanner.nextLine();
+                
+                System.out.println(INDENTBLOCK + DIVIDER);
+
+                if (command.equals("bye")) {
+                    System.out.println(INDENTBLOCK + "Bye, see you later!");
+                    System.out.println(INDENTBLOCK + DIVIDER);
+                    break;
+                }
+                
+                System.out.println(INDENTBLOCK + command);
+                System.out.println(INDENTBLOCK + DIVIDER);
+            }
+        }
     }
 }
