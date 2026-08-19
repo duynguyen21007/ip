217a0b980b367693f21706bf608172a3a9ec9792
Duy Nguyen <duynguyen21007@gmail.com>
2026-08-19 19:12:01 +0800

    Rename Chatbot, add greetings, decorations

---
+++ src/main/java/Milo.java
@@ -0,0 +1,15 @@
+public class Milo {
+    public static void main(String[] args) {
+        String chatbotName = "Milo";
+        String greeting = "============================================================\n"
+                + "Hello! I'm " + chatbotName + ".\n"
+                + "How can I help you?\n"
+                + "============================================================\n";
+        String banner = " __  __ _ _       \n"
+                      + "|  \\/  (_) | ___  \n"
+                      + "| |\\/| | | |/ _ \\ \n"
+                      + "| |  | | | | (_) |\n"
+                      + "|_|  |_|_|_|\\___/ \n";
+        System.out.println(banner + greeting);
+    }
+}
