ab2cbc4066617065e1016690ba89d8fcef6c8348
Duy Nguyen <duynguyen21007@gmail.com>
2026-08-19 21:33:27 +0800

    Add add, list feature

--- src/main/java/Milo.java
+++ src/main/java/Milo.java
@@ -1,8 +1,13 @@
 import java.util.Scanner;
 
+/**
+ * Runs the Milo chatbot and manages its in-memory task list.
+ */
 public class Milo {
     private static final String DIVIDER = "-----------------------------------";
     private static final String INDENTBLOCK = "                   ";
+    private static final String[] TASKS = new String[100];
+
     public static void main(String[] args) {
         String chatbotName = "Milo";
         String greeting = DIVIDER + "\n"
@@ -17,18 +22,26 @@ public class Milo {
         System.out.println(banner + greeting);
 
         try (Scanner scanner = new Scanner(System.in)) {
+            int taskCount = 0;
             while (scanner.hasNextLine()) {
                 String command = scanner.nextLine();
-                
+
                 System.out.println(INDENTBLOCK + DIVIDER);
 
                 if (command.equals("bye")) {
                     System.out.println(INDENTBLOCK + "Bye, see you later!");
                     System.out.println(INDENTBLOCK + DIVIDER);
                     break;
+                } else if (command.equals("list")) {
+                    for (int i = 0; i < taskCount; i++) {
+                        System.out.println(INDENTBLOCK + (i + 1) + ". " + TASKS[i]);
+                    }
+                } else {
+                    TASKS[taskCount] = command;
+                    taskCount++;
+                    System.out.println(INDENTBLOCK + "added: " + command);
                 }
-                
-                System.out.println(INDENTBLOCK + command);
+
                 System.out.println(INDENTBLOCK + DIVIDER);
             }
         }
