97c389eec7d40cc8f33c84278d869b18017efecb
Duy Nguyen <duynguyen21007@gmail.com>
2026-08-20 20:15:38 +0800

    Add mark done

--- src/main/java/Milo.java
+++ src/main/java/Milo.java
@@ -7,6 +7,8 @@ public class Milo {
     private static final String DIVIDER = "-----------------------------------";
     private static final String INDENTBLOCK = "                   ";
     private static final String[] TASKS = new String[100];
+    /** Stores the completion state for the task at the matching index in {@link #TASKS}. */
+    private static final boolean[] TASKS_DONE = new boolean[TASKS.length];
 
     public static void main(String[] args) {
         String chatbotName = "Milo";
@@ -33,9 +35,13 @@ public class Milo {
                     System.out.println(INDENTBLOCK + DIVIDER);
                     break;
                 } else if (command.equals("list")) {
+                    System.out.println(INDENTBLOCK + "Here are the tasks in your list:");
                     for (int i = 0; i < taskCount; i++) {
-                        System.out.println(INDENTBLOCK + (i + 1) + ". " + TASKS[i]);
+                        String statusIcon = TASKS_DONE[i] ? "X" : " ";
+                        System.out.println(INDENTBLOCK + (i + 1) + ".[" + statusIcon + "] " + TASKS[i]);
                     }
+                } else if (command.equals("mark") || command.startsWith("mark ")) {
+                    markTask(command, taskCount);
                 } else {
                     TASKS[taskCount] = command;
                     taskCount++;
@@ -45,5 +51,33 @@ public class Milo {
                 System.out.println(INDENTBLOCK + DIVIDER);
             }
         }
+    }
+
+    /**
+     * Marks the task number supplied in a command such as {@code mark 2} as done.
+     *
+     * @param command full command entered by the user
+     * @param taskCount number of tasks currently stored
+     */
+    private static void markTask(String command, int taskCount) {
+        String taskNumberText = command.substring("mark".length()).trim();
+        int taskNumber;
+
+        try {
+            taskNumber = Integer.parseInt(taskNumberText);
+        } catch (NumberFormatException exception) {
+            System.out.println(INDENTBLOCK + "Please specify a task number, for example: mark 2");
+            return;
+        }
+
+        if (taskNumber < 1 || taskNumber > taskCount) {
+            System.out.println(INDENTBLOCK + "There is no task numbered " + taskNumber + ".");
+            return;
+        }
+
+        int taskIndex = taskNumber - 1;
+        TASKS_DONE[taskIndex] = true;
+        System.out.println(INDENTBLOCK + "Nice! I've marked this task as done:");
+        System.out.println(INDENTBLOCK + "  [X] " + TASKS[taskIndex]);
     }
 }
