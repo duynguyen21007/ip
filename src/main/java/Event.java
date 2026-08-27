27a2157cd0ef098cfd1c62f97759c868c6825988
Duy Nguyen <duynguyen21007@gmail.com>
2026-08-27 23:08:56 +0800

    Implement Level 8 date handling

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,18 +1,22 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 /**
  * Represents a task that occurs between a specific start and end date or time.
  */
 public class Event extends Task {
-    protected String from;
-    protected String to;
+    private static final DateTimeFormatter DISPLAY_FORMAT = DateTimeFormatter.ofPattern("MMM dd yyyy");
+    protected LocalDate from;
+    protected LocalDate to;
 
     /**
      * Creates an incomplete event with its start and end date or time.
      *
      * @param description description of the event
-     * @param from start date or time
-     * @param to end date or time
+    * @param from start date
+    * @param to end date
      */
-    public Event(String description, String from, String to) {
+    public Event(String description, LocalDate from, LocalDate to) {
         super(description);
         this.from = from;
         this.to = to;
@@ -25,6 +29,7 @@ public class Event extends Task {
      */
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
+        return "[E]" + super.toString() + " (from: " + from.format(DISPLAY_FORMAT)
+            + " to: " + to.format(DISPLAY_FORMAT) + ")";
     }
 }
